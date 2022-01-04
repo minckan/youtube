15c9c64ff365065bfcff0ae09aa58476b3e7f23c
minckan <mymy317@naver.com>
2022-01-04 22:30:32 +0900

    add youtube video detail

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,21 @@ import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './Components/video_list/video_list';
 import Header from './Components/header/header';
+import VideoDetail from './Components/video_detail/video_detail';
 
 function App({youtube}) {
     const [videos, setVideo] = useState([])
+    const [selectedVideo, setSelectVideo] = useState(null)
+
+    const selectVideo = (video) => {
+        setSelectVideo(video)
+    }
 
     const handleSearch = query => {
-        youtube.search(query).then(videos => setVideo(videos))
+        setSelectVideo(null)
+        youtube.search(query).then(videos => {
+            setVideo(videos)
+        })
     }
 
     useEffect(() => {
@@ -17,8 +26,15 @@ function App({youtube}) {
 
     return (
         <div className={ styles.app}>
-            <Header onSearch={ handleSearch }></Header>
-            <VideoList videos={videos}></VideoList>
+            <Header onSearch={handleSearch}></Header>
+            <section className={styles.content}>
+                {
+                    selectedVideo && (
+                        <div className={styles.detail}> <VideoDetail video={selectedVideo}></VideoDetail></div>
+                    )
+                }
+                <div className={styles.list}><VideoList videos={videos} onVideoClick={ selectVideo} display={selectedVideo ? 'list' : 'grid'}></VideoList></div>
+            </section>
         </div>
     );
 }
